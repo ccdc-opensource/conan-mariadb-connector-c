35775daedc5b06325ee74e44efdc081a3d39dce9
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2021-04-22 12:09:09 +0000

    Update openssl and curl

--- conanfile.py
+++ conanfile.py
@@ -41,11 +41,11 @@ class MariadbConnectorConan(ConanFile):
 
     def requirements(self):
         if self.options.with_ssl:
-            self.requires("openssl/1.1.1i")
+            self.requires("openssl/1.1.1k")
         if self.options.with_external_zlib:
             self.requires("zlib/1.2.11")
         if self.options.with_curl:
-            self.requires("libcurl/7.74.0")
+            self.requires("libcurl/7.75.0")
 
     def build(self):
         cmake = CMake(self)
